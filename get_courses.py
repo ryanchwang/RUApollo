63224dfc7ff1a686eb7e007af9ab0dab31bb7185
ryanchwang <ryanchwang@gmail.com>
2025-02-01 15:18:48 -0500

    Update get_courses.py

--- get_courses.py
+++ get_courses.py
@@ -4,24 +4,18 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 
-driver = webdriver.Chrome()
-
-df = pd.read_csv('subjects.csv')
+df = pd.read_csv('subjects.csv', dtype = str)
 n = len(df)
-
-print(df)
 
 courses = []
 
-for i in range(5):
+for i in n:
 
-    print(i)
-
-    scode = df["code"].iloc[i]
-
+    scode = df["code"].iloc[i].strip()
+    driver = webdriver.Chrome()
     url = f'https://classes.rutgers.edu/soc/#courses?subject={scode}&semester=12025&campus=NB&level=U'
     response = driver.get(url)
-    time.sleep(10)
+    time.sleep(3)
     html_content = driver.page_source
 
     soup = BeautifulSoup(html_content, 'html.parser')
@@ -29,17 +23,22 @@ for i in range(5):
     course_data = []
 
     # Find all course IDs and Titles
-    courseID_elements = soup.find_all(class_='courseID')
+    courseID_elements = soup.find_all(class_='courseId')
     courseTitle_elements = soup.find_all(class_='courseTitle')
+    courseCredit_elements = soup.find_all(class_='courseCredits')
     
-    for courseID, courseTitle in zip(courseID_elements, courseTitle_elements):
+    for courseID, courseTitle, courseCredit in zip(courseID_elements, courseTitle_elements, courseCredit_elements):
         course_data.append({
             'courseID': courseID.text.strip(),
-            'courseTitle': courseTitle.text.strip()
+            'courseTitle': courseTitle.text.strip(),
+            'courseCredit': courseCredit.text.strip()
         })
     
     # Append this data for the current row
-    courses.append(course_data)
+    if course_data: courses.append(course_data)
+
+    driver.quit()
+
 
 print(courses)
 
